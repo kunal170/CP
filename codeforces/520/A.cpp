1fbb6eae6af40ce286400ea4a4bc85f639f253de
Kunal Thite <thite2001@gmail.com>
2021-06-01 19:13:00 +0530

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/118096030

---
+++ codeforces/520/A.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int i,n;
+cin>>n;
+string s;
+cin>>s;
+int H[26]={0};
+bool c=true;
+for(i=0;i<n;i++){
+    if(s[i]>=97)
+    H[s[i]-97]++;
+    else
+    H[s[i]-65]++;
+}
+for(i=0;i<26;i++){
+    if(H[i]==0){
+        c=false;
+        break;
+    }
+}
+if(c)
+cout<<"YES\n";
+else
+cout<<"NO\n";
+return 0;
+}
