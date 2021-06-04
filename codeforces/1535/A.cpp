e8bf5d719929e671983954f7a872ff6c6d454c39
Kunal Thite <thite2001@gmail.com>
2021-06-04 20:13:00 +0530

    Add solution for problem `A - Fair Playoff` Link: https://codeforces.com/contest/1535/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1535/submission/118380957

---
+++ codeforces/1535/A.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+int i,n,x,y,c,d;
+int A[4];
+for(i=0;i<4;i++)
+cin>>A[i];
+if(A[0]>A[1]){
+x=A[0];
+y=A[1];
+}else{
+    x=A[1];
+    y=A[0];
+}
+if(A[2]>A[3]){
+    c=A[2];
+    d=A[3];
+}
+else{
+    c=A[3];
+    d=A[2];
+}
+
+if(y>c || d>x)
+cout<<"NO\n";
+else
+cout<<"YES\n";
+}
+return 0;
+}
