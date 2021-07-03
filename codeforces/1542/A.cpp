86bbf8c78b451d59bec9068f0a319be4f76ac024
Kunal Thite <thite2001@gmail.com>
2021-07-03 18:43:00 +0530

    Add solution for problem `A - Odd Set` Link: https://codeforces.com/contest/1542/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1542/submission/121199961

---
+++ codeforces/1542/A.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+int i,m,o=0,e=0,x;
+cin>>m;
+int z=0;
+z++;
+for(i=0;i<2*m;i++){
+    cin>>x;
+    if(x%2)
+    o++;
+    else
+    e++;
+}
+if(o==e)
+cout<<"Yes\n";
+else
+cout<<"No\n";
+}
+return 0;
+}
