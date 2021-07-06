d88096748accc1b61c71e7858a6d9354c58fe7c1
Kunal Thite <thite2001@gmail.com>
2021-07-06 07:13:00 +0530

    Add solution for problem `A - String Similarity` Link: https://codeforces.com/contest/1400/problem/A Tags: constructive algorithms, strings, *800 Ref: https://codeforces.com/contest/1400/submission/121435491

---
+++ codeforces/1400/A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+int i,n;
+cin>>n;
+string s,a;
+cin>>s;
+i=0;
+while(i<2*n-1){
+    a.push_back(s[i]);
+    i+=2;
+}
+cout<<a<<'\n';
+}
+return 0;
+}
