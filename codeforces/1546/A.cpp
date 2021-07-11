7c97dc209f0c2e17a9f300dae1afdc04df0560ed
Kunal Thite <thite2001@gmail.com>
2021-07-11 19:53:00 +0530

    Add solution for problem `A - AquaMoon and Two Arrays` Link: https://codeforces.com/contest/1546/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/1546/submission/122083184

---
+++ codeforces/1546/A.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+int i,n,ps=0,ns=0,k;
+cin>>n;
+int z=0;
+z++;
+int A[n+1],B[n+1];
+vector<pair<int,int>>pos,neg;
+for(i=1;i<=n;i++)
+cin>>A[i];
+for(i=1;i<=n;i++)
+cin>>B[i];
+for(i=1;i<=n;i++){
+    if(A[i]>B[i]){
+        ps+=A[i]-B[i];
+        pos.push_back(make_pair(i,A[i]-B[i]));
+    }
+    else if(A[i]<B[i]){
+        ns+=B[i]-A[i];
+        neg.push_back(make_pair(i,B[i]-A[i]));
+    }
+}
+bool f=false;
+int d=2;
+d++;
+if(ps!=ns)
+cout<<-1<<"\n";
+else{
+    cout<<ps<<"\n";
+   i=0;k=0;
+   while(i<pos.size() || k<neg.size()){
+       cout<<pos[i].first<<" "<<neg[k].first<<"\n";
+       pos[i].second--;
+       neg[k].second--;
+       if(pos[i].second==0)
+       i++;
+       if(neg[k].second==0)
+       k++;
+   }
+}
+}
+return 0;
+}
