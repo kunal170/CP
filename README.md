2dede133d69f9e72567b8a4b30740b6388a82cf3
Kunal Thite <thite2001@gmail.com>
2022-02-02 19:35:00 +0530

    Add solution for problem `A - Arrival of the General` Link: https://codeforces.com/contest/144/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/144/submission/144946396

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Submissions
 ## Introduction
 
 A repository to keep track of problem solving practice, containing solutions from platforms:
-{profile_placeholder}
+* Codeforces &nbsp; [![Codeforces](https://run.kaist.ac.kr/badges/codeforces/kunalthite.svg)](https://codeforces.com/profile/kunalthite)
+
 
 ## Contents
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-{submission_placeholder}
+1 | [A - Arrival of the General](https://codeforces.com/contest/144/problem/A) | [GNU C++17](./codeforces/144/A.cpp) | `implementation` `*800` | Feb/02/2022 19:35 | 
---
+++ codeforces/144/A.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int i,n,maxi,mini,j,c=0,k;
+cin>>n;
+int A[n];
+for(i=0;i<n;i++)
+cin>>A[i];
+j=k=0;
+maxi=mini=A[0];
+for(i=1;i<n;i++){
+	if(A[i]>maxi){
+		maxi=A[i];
+		j=i;
+	}
+	if(A[i]<=mini){
+		mini=A[i];
+		k=i;
+	}
+}
+c+=j+n-1-k;
+if(k<=j)
+c--;
+cout<<c<<"\n";
+
+return 0;
+}
