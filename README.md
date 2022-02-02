ca561a6ba91a8c7a13ec4cb431ef9761aa16c900
Kunal Thite <thite2001@gmail.com>
2022-02-02 19:26:00 +0530

    Add solution for problem `A - Anton and Polyhedrons` Link: https://codeforces.com/contest/785/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/785/submission/144945363

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1 | [A - Arrival of the General](https://codeforces.com/contest/144/problem/A) | [GNU C++17](./codeforces/144/A.cpp) | `implementation` `*800` | Feb/02/2022 19:35 | 
+2 | [A - Arrival of the General](https://codeforces.com/contest/144/problem/A) | [GNU C++17](./codeforces/144/A.cpp) | `implementation` `*800` | Feb/02/2022 19:35 | 
+1 | [A - Anton and Polyhedrons](https://codeforces.com/contest/785/problem/A) | [GNU C++17](./codeforces/785/A.cpp) | `implementation` `strings` `*800` | Feb/02/2022 19:26 | 
---
+++ codeforces/785/A.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod        1000000007
+#define pb         push_back
+#define mp         make_pair
+#define rep(n)     for(int i=0;i<n;i++)
+#define repit(x)   for(auto it : x)
+#define ll         long long
+#define vi         vector<int>
+#define vc         vector<char>
+#define vs         vector<string>
+#define mii        map<int ,int>
+#define mci        map<char ,int>
+/* bool prime[100001];
+ const int N=100000;
+ void sieve() {
+ int i,j;
+for(i=2;i<=N;i++)
+prime[i]=true;
+ prime[0]=prime[1]=false;
+for(i=2;i*i<=N;i++){
+if(prime[i]){
+for(j=i*i;j<=N;j+=i)
+prime[j]=false;
+}
+}
+}*/
+int gcd(int a,int b){
+if(b==0)
+return a;
+else
+return gcd(b,a%b);
+}
+void swap(int *x,int *y){
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+    
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+ll i,n,sum=0;
+string s;
+cin>>n;
+for(i=0;i<n;i++){
+	cin>>s;
+	if(s=="Tetrahedron")
+	sum+=4;
+	else if(s=="Cube")
+	sum+=6;
+	else if(s=="Octahedron" )
+	sum+=8;
+	else if(s=="Dodecahedron")
+	sum+=12;
+	else
+	sum+=20;
+}
+cout<<sum<<"\n";
+
+return 0;
+}
